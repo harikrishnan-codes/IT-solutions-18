27874db09fabacffb30e88fcb636989c8f34a85f
HariKrishnan <krish96882@gmail.com>
2026-02-25 18:29:39 +0530

    Add files via upload

--- signup.js
+++ signup.js
@@ -46,3 +46,21 @@ document.getElementById("forgotForm").addEventListener("submit", function (e) {
     e.preventDefault();
     validateAndRedirect(["forgotEmail"], "forgotError");
 });
+
+
+
+
+
+
+
+
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("loginPassword");
+
+togglePassword.addEventListener("click", function () {
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    this.classList.toggle("fa-eye");
+    this.classList.toggle("fa-eye-slash");
+});
